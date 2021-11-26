94f767f02002ded63d4c0e64d74ce026f2c29cfe
Kaue Alves <kaue_alves00@yahoo.com>
2021-11-26 16:48:31 -0300

    conhecendo o javaScript

---
+++ fundamentos/bloco_4_introdução_à_javascript_e_lógica_de_programação/4.1_introdução_primeiros_passos/scripts.js
@@ -0,0 +1,45 @@
+/* const myName = 'Kaue';
+const birthCity = 'mauá';
+let birthYear = 1995;
+
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear); */
+
+/* let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge); */
+
+/* const base = 5;
+const height = 8;
+const area = (base*height);
+const perimeter = (2*base + 2*height);
+
+console.log(base);
+console.log(height);
+console.log(area);
+console.log(perimeter); */
+
+const note = 79;
+
+if (note >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (note<80 && note>=60) {
+  console.log("Você está na nossa lista de espera");
+}
+else {
+  console.log("Você foi reprovada(o)");
+}
