94a1edb70f430c34f191b0d47fa9c6043ac04ee3
Kaue Alves <kaue_alves00@yahoo.com>
2021-11-29 19:29:57 -0300

    concluindo o 2º, 3º, 4º, e 5º exercicio

--- fundamentos/bloco_4_introdução_à_javascript_e_lógica_de_programação/4.2_array_e_loop_for/exercicios.js
+++ fundamentos/bloco_4_introdução_à_javascript_e_lógica_de_programação/4.2_array_e_loop_for/exercicios.js
@@ -1,6 +1,49 @@
 // 1
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let i = 0; i < numbers.length; i += 1) {
-  console.log (numbers[i])
-}
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log (numbers[i])
+// }
+
 // 2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   soma += numbers[i];
+// }
+// console.log(soma);
+
+// 3
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   soma += numbers[i];
+// }
+// console.log(médiaA = soma / numbers.length)
+
+// 4
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   soma += numbers[i];
+// }
+// let médiaA = soma / numbers.length;
+//   console.log (médiaA)
+//   if (médiaA > 20) {
+//     console.log('maior que 20')
+//   }else {
+//     console.log('menor que 20')
+//   }
+
+// 5
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let maiorNumero = numbers[0]
+
+// for (i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] > maiorNumero) {
+//     maiorNumero = numbers[i]
+//   }
+// }
+// console.log(maiorNumero)
