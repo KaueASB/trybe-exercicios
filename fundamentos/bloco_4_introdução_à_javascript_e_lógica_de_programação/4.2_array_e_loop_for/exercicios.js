372026800f10e291c43a3348d1b5468179325328
Kaue Alves <kaue_alves00@yahoo.com>
2021-11-29 22:00:19 -0300

    concluindo o setimo exercicio

--- fundamentos/bloco_4_introdução_à_javascript_e_lógica_de_programação/4.2_array_e_loop_for/exercicios.js
+++ fundamentos/bloco_4_introdução_à_javascript_e_lógica_de_programação/4.2_array_e_loop_for/exercicios.js
@@ -63,3 +63,14 @@
 //  } else {
 //     console.log(impar);
 //   }
+
+// 7
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorNumero = numbers[1]
+
+for (i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] < menorNumero) {
+    menorNumero = numbers[i]
+  }
+}
+console.log(menorNumero)
