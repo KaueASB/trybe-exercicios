c7ee8c685caf78df7665d3490475652865f4a7e2
Kaue Alves <kaue_alves00@yahoo.com>
2021-11-30 20:07:58 -0300

    atualizando arquivo

--- fundamentos/bloco_4_introdução_à_javascript_e_lógica_de_programação/4.3_logica_e_algoritmos/exercicios.js
+++ fundamentos/bloco_4_introdução_à_javascript_e_lógica_de_programação/4.3_logica_e_algoritmos/exercicios.js
@@ -1,11 +1,34 @@
-let number = 3
-let simbolo = '*'
-let iColumn = '';
+// exercicio 1
+// let number = 3
+// let asterisco = '*'
+// let colunas = '';
 
-for (i = 0; i < number; i += 1) {
-  iColumn = iColumn + simbolo;
-}
+// for (i = 0; i < number; i += 1) {
+//   colunas = colunas + asterisco;
+// }
 
-for (i = 0; i < number; i += 1) {
-  console.log (iColumn)
+// for (i = 0; i < number; i += 1) {
+//   console.log (colunas)
+// }
+
+/*  */
+
+// exercicio 2
+// let size = 5
+// let asterisco = '*'
+// let colunas = '';
+
+// for (i = 0; i <= size; i += 1) {
+//   console.log (colunas);
+//   colunas = colunas + asterisco;
+// }
+
+// exercicio 3
+let tamanho = 5
+let asterisco = '*'
+let colunas = '';
+
+for (i = 0; i <= size; i += 1) {
+  console.log (colunas);
+  colunas = colunas + asterisco;
 }
