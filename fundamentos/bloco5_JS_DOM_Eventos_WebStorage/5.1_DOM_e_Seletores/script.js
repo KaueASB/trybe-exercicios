1eec3a3cc7a6655fb45a62847f94be986c38f78f
Kaue Alves <kaue_alves00@yahoo.com>
2021-12-06 14:50:03 -0300

    atualizando os nomes dos diretorios

---
+++ fundamentos/bloco5_JS_DOM_Eventos_WebStorage/5.1_DOM_e_Seletores/script.js
@@ -0,0 +1,14 @@
+const header = document.getElementById("header-container");
+header.style.backgroundColor = "black";
+
+const urgente = document.getElementsByClassName("emergency-tasks")[0];
+urgente.style.backgroundColor = "red";
+
+const notUrgente = document.getElementsByClassName("no-emergency-tasks")[0];
+notUrgente.style.backgroundColor = "blue";
+
+
+
+
+const footer = document.querySelector("#footer-container");
+footer.style.backgroundColor = "black";
