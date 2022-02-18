b70c429bae0d4cd4731cde0b9ba9209fadee4bc3
Kaue Alves <kaue_alves00@yahoo.com>
2022-02-17 22:38:10 -0300

    realizando os exec de fixaçao 10.2

---
+++ desenvolvimento_front-end/bloco10_introdução_à_React/10.2_componentes_react/exec_fixation/src/components/UserProfile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from './Image';
+import PropTypes from 'prop-types'
+
+class UserProfile extends React.Component {
+  render() {
+    const { user: { name, email, avatar } } = this.props;
+    return (
+      <div>
+        <p> {name} </p>
+        <p> {email} </p>
+        <Image source={avatar} alternativeText="User avatar" />
+      </div>
+    );
+  }
+}
+
+UserProfile.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    avatar: PropTypes.string,
+  }).isRequired,
+};
+
+export default UserProfile;
