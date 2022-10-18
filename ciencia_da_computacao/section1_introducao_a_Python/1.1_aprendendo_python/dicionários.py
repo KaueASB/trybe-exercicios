57ae253d50ef28f51e1f4dc0c9daf1f14e2401c8
Kaue Alves <kaue_alves00@yahoo.com>
2022-10-18 10:20:49 -0300

    add libs flake8 and black

--- ciencia_da_computacao/section1_introducao_a_Python/1.1_aprendendo_python/dicionários.py
+++ ciencia_da_computacao/section1_introducao_a_Python/1.1_aprendendo_python/dicionários.py
@@ -2,9 +2,17 @@
 
 # Estrutura que associa uma chave a um determinado valor. É a representação do tão famoso objeto que utilizamos em JavaScript.
 
-people_by_id = {1: "Maria", 2: "Fernanda", 3: "Felipe"}  # elementos no formato "chave: valor" separados por vírgula, envolvidos por chaves
+people_by_id = {
+    1: "Maria",
+    2: "Fernanda",
+    3: "Felipe",
+}  # elementos no formato "chave: valor" separados por vírgula, envolvidos por chaves
 
-people_by_name = {"Maria": 1, "Fernanda": 2, "Felipe": 3}  # outro exemplo, dessa vez usando strings como chaves. As aspas são necessárias para que o Python não ache que `Maria`, `Fernanda` e `Felipe` sejam variáveis.
+people_by_name = {
+    "Maria": 1,
+    "Fernanda": 2,
+    "Felipe": 3,
+}  # outro exemplo, dessa vez usando strings como chaves. As aspas são necessárias para que o Python não ache que `Maria`, `Fernanda` e `Felipe` sejam variáveis.
 
 # elementos são acessados por suas chaves
 people_by_id[1]  # saída: Maria
@@ -16,9 +24,9 @@ people_by_id.items()  # dict_items([(1, "Maria"), (2, "Fernanda"), (3, "Felipe")
 # um conjunto é retornado com tuplas contendo chaves e valores
 
 info = {
-  "personagem": "Margarida",
-  "origem": "Pato Donald",
-  "nota": "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    "personagem": "Margarida",
+    "origem": "Pato Donald",
+    "nota": "Namorada do personagem principal nos quadrinhos do Pato Donald",
 }
 
 info["recorrente"] = "Sim"
